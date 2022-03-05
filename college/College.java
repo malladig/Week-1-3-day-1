33fcd97632d0e38373841ab5b357866a816c5369
malladig <100071526+malladig@users.noreply.github.com>
2022-03-05 23:31:28 +0530

    Add files via upload

---
+++ college/College.java
@@ -0,0 +1,13 @@
+package org.college;
+
+public class College {
+	public void collegeName() {
+		System.out.println("college name is jntu");
+	}
+    public void collegeCode() {
+    	System.out.println("college code is 1");
+    }
+    public void collegeRank() {
+    	System.out.println("college rank is 100");
+    }
+}
