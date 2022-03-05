c08728255ec3b3818726e3814f670ad7b90b6b2a
malladig <100071526+malladig@users.noreply.github.com>
2022-03-05 23:31:44 +0530

    Add files via upload

---
+++ department/Department.java
@@ -0,0 +1,10 @@
+package org.department;
+
+import org.college.College;
+
+public class Department extends College {
+	public void departName() {
+		System.out.println("dept name is computers");
+	}
+
+}
