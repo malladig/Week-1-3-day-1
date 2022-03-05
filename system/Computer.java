cd9767448a7f21d796f034128c29e04f2c9091e1
malladig <100071526+malladig@users.noreply.github.com>
2022-03-05 23:30:15 +0530

    Add files via upload

---
+++ system/Computer.java
@@ -0,0 +1,23 @@
+package org.system;
+
+public class Computer {
+	
+public	void computerModel() {
+		System.out.println("computermodel");
+		
+	}
+
+}
+	
+	
+
+	 
+
+
+		
+
+	
+		
+
+	
+
---
+++ system/simpleInheritance.java
@@ -0,0 +1,14 @@
+package org.system;
+
+public class simpleInheritance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Desktop D=new Desktop();
+		D.computerModel();
+		D.desktopSize();
+	
+	}
+
+}
